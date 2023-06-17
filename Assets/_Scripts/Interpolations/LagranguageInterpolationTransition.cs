2352fbfea2db922b4363d2ea58de1df63d5211e9
Marek-Laptop <maro.990@gmail.com>
2023-06-17 13:28:50 +0200

    Laagranguage optymalisation

--- Assets/_Scripts/Interpolations/LagranguageInterpolationTransition.cs
+++ Assets/_Scripts/Interpolations/LagranguageInterpolationTransition.cs
@@ -10,12 +10,104 @@ public class LagranguageInterpolationTransition : BaseInterpolationScript
         rotationMethod = LagranguageInterpolation;
     }
 
+    //private Quaternion LagranguageInterpolation(float currentTransitionT, List<InterpolationNode> nodes)
+    //{
+    //    Quaternion returning = Quaternion.identity;
+    //    float axisResult = 0;
+    //    float f1 = 1, f2 = 1;
+
+    //    for (var i = 0; i < nodes.Count; i++)
+    //    {
+    //        f1 = f2 = 1;
+    //        for (var j = 0; j < nodes.Count; j++)
+    //        {
+    //            if (i != j)
+    //            {
+    //                f1 *= currentTransitionT - nodes[j].frame;
+    //                f2 *= nodes[i].frame - nodes[j].frame;
+    //            }
+    //        }
+
+    //        if (f2 != 0)
+    //            axisResult += nodes[i].rotation.x * f1 / f2;
+    //        else
+    //            axisResult = nodes[0].rotation.x;
+    //    }
+    //    returning.x = axisResult;
+    //    axisResult = 0;
+
+    //    for (var i = 0; i < nodes.Count; i++)
+    //    {
+    //        f1 = f2 = 1;
+    //        for (var j = 0; j < nodes.Count; j++)
+    //        {
+    //            if (i != j)
+    //            {
+    //                f1 *= currentTransitionT - nodes[j].frame;
+    //                f2 *= nodes[i].frame - nodes[j].frame;
+    //            }
+    //        }
+
+    //        if (f2 != 0)
+    //            axisResult += nodes[i].rotation.y * f1 / f2;
+    //        else
+    //            axisResult = nodes[0].rotation.y;
+    //    }
+    //    returning.y = axisResult;
+    //    axisResult = 0;
+
+    //    for (var i = 0; i < nodes.Count; i++)
+    //    {
+    //        f1 = f2 = 1;
+    //        for (var j = 0; j < nodes.Count; j++)
+    //        {
+    //            if (i != j)
+    //            {
+    //                f1 *= currentTransitionT - nodes[j].frame;
+    //                f2 *= nodes[i].frame - nodes[j].frame;
+    //            }
+    //        }
+
+    //        if (f2 != 0)
+    //            axisResult += nodes[i].rotation.z * f1 / f2;
+    //        else
+    //            axisResult = nodes[0].rotation.z;
+    //    }
+    //    returning.z = axisResult;
+    //    axisResult = 0;
+
+    //    for (var i = 0; i < nodes.Count; i++)
+    //    {
+    //        f1 = f2 = 1;
+    //        for (var j = 0; j < nodes.Count; j++)
+    //        {
+    //            if (i != j)
+    //            {
+    //                f1 *= currentTransitionT - nodes[j].frame;
+    //                f2 *= nodes[i].frame - nodes[j].frame;
+    //            }
+    //        }
+
+    //        if (f2 != 0)
+    //            axisResult += nodes[i].rotation.w * f1 / f2;
+    //        else
+    //            axisResult = nodes[0].rotation.w;
+    //    }
+    //    returning.w = axisResult;
+    //    axisResult = 0;
+
+    //    return returning;
+    //}
+
     private Quaternion LagranguageInterpolation(float currentTransitionT, List<InterpolationNode> nodes)
     {
         Quaternion returning = Quaternion.identity;
         float axisResult = 0;
-        float f1 = 1,f2 = 1;
-        
+        float f1, f2;
+
+        float[] d = new float[nodes.Count];
+
+
         for (var i = 0; i < nodes.Count; i++)
         {
             f1 = f2 = 1;
@@ -26,75 +118,40 @@ public class LagranguageInterpolationTransition : BaseInterpolationScript
                     f1 *= currentTransitionT - nodes[j].frame;
                     f2 *= nodes[i].frame - nodes[j].frame;
                 }
-            }
 
-            if (f2 != 0)
-                axisResult += nodes[i].rotation.x * f1 / f2;
-            else
-                axisResult = nodes[0].rotation.x;
+                if (f2 != 0)
+                    d[i] = f1 / f2;
+            }
         }
-        returning.x = axisResult;
-        axisResult = 0;
-        
-        for (var i = 0; i < nodes.Count; i++)
-        {
-            f1 = f2 = 1;
-            for (var j = 0; j < nodes.Count; j++)
-            {
-                if (i != j)
-                {
-                    f1 *= currentTransitionT - nodes[j].frame;
-                    f2 *= nodes[i].frame - nodes[j].frame;
-                }
-            }
 
-            if (f2 != 0)
-                axisResult += nodes[i].rotation.y * f1 / f2;
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            if(d[i] != 0)
+            {
+                returning.x += nodes[i].rotation.x * d[i];
+                returning.y += nodes[i].rotation.y * d[i];
+                returning.z += nodes[i].rotation.z * d[i];
+               // returning.w += nodes[i].rotation.w * d[i];
+            }
             else
-                axisResult = nodes[0].rotation.y;
-        }
-        returning.y = axisResult;
-        axisResult = 0;
-        
-        for (var i = 0; i < nodes.Count; i++)
-        {
-            f1 = f2 = 1;
-            for (var j = 0; j < nodes.Count; j++)
             {
-                if (i != j)
-                {
-                    f1 *= currentTransitionT - nodes[j].frame;
-                    f2 *= nodes[i].frame - nodes[j].frame;
-                }
+                returning.x = nodes[0].rotation.x;
+                returning.y = nodes[0].rotation.y;
+                returning.z = nodes[0].rotation.z;
+                //returning.w = nodes[0].rotation.w;
             }
 
-            if (f2 != 0)
-                axisResult += nodes[i].rotation.z * f1 / f2;
-            else
-                axisResult = nodes[0].rotation.z;
         }
-        returning.z = axisResult;
-        axisResult = 0;
-        
+
         for (var i = 0; i < nodes.Count; i++)
         {
-            f1 = f2 = 1;
-            for (var j = 0; j < nodes.Count; j++)
-            {
-                if (i != j)
-                {
-                    f1 *= currentTransitionT - nodes[j].frame;
-                    f2 *= nodes[i].frame - nodes[j].frame;
-                }
-            }
 
-            if (f2 != 0)
-                axisResult += nodes[i].rotation.w * f1 / f2;
+            if (d[i] != 0)
+                axisResult += nodes[i].rotation.w * d[i];
             else
                 axisResult = nodes[0].rotation.w;
         }
         returning.w = axisResult;
-        axisResult = 0;
 
         return returning;
     }
